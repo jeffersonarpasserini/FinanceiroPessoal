6b34cd8c95a88f517a82dcd0d3eb7efcc0fd6fcb
jeffersonarpasserini <jefferson.passerini@unesp.br>
2022-10-09 19:52:33 -0300

    initial commit financeiropessoal_back

---
+++ FinanceiroPessoal_Back/src/main/java/com/cursospring/FinanceiroPessoal/FinanceiroPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.FinanceiroPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroPessoalApplication.class, args);
+	}
+
+}
---
+++ FinanceiroPessoal_Back/src/test/java/com/cursospring/FinanceiroPessoal/FinanceiroPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.FinanceiroPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
